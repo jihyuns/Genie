8fd8e7da32d72fafc9b5f13fd17fd475592ea7a1
YoungHunCho <cyh905@nate.com>
2016-03-24 23:29:23 +0900

    TTS
    .

--- src/cppProject/Genie/header/Genie_Speech.h
+++ src/cppProject/Genie/header/Genie_Speech.h
@@ -1,16 +1,24 @@
 #ifndef __GENIE_SPEECH__
 #define __GENIE_SPEECH__
 
+
+
+
 class Genie_Speech
 {
 	private:
 	/*	...
 		if you want add on
 	*/
+		// 03.24 by cho
+		std::wstring stws(const std::string& _src);
+		
 	public:
 	/*	...
 		if you want add on
 	*/	
+		// 03.24 by cho
+		int text_to_speech(const std::string text);
 };
 
 #endif
--- src/cppProject/Genie/src/Genie_Speech.cpp
+++ src/cppProject/Genie/src/Genie_Speech.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <cstring>
 #include "../header/Genie_Speech.h"
+// 03.24 by cho
+// tts
+#include <sapi.h>
+#include <atlconv.h>
 using namespace std;
 
 
@@ -14,3 +18,30 @@ using namespace std;
 	
 	}
 */
+
+// 03.24 by cho
+std::wstring Genie_Speech::stws(const std::string& _src) {
+	USES_CONVERSION;
+	return std::wstring(A2W(_src.c_str()));
+}
+
+//usage  text_to_speech("¾È³ç");
+//or std::string a = "¾È³ç";  text_to_speech(a);
+int Genie_Speech::text_to_speech(const std::string text) {
+	std::wstring Ltext = stws(text.c_str());
+
+	ISpVoice * pVoice = NULL;
+	if (FAILED(::CoInitialize(NULL)))		return FALSE;
+	HRESULT hr = CoCreateInstance(CLSID_SpVoice, NULL, CLSCTX_ALL, IID_ISpVoice, (void **)&pVoice);
+	if (SUCCEEDED(hr))	{
+		LPCWSTR str = Ltext.c_str();
+		hr = pVoice->Speak(Ltext.c_str(), 0, NULL);//SPF_IS_XML
+		pVoice->Release();
+		pVoice = NULL;
+	}
+	else {
+		return -1;
+	}
+	::CoUninitialize();
+	return 1;
+}
