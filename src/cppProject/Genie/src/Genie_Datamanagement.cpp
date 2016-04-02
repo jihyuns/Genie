f6eedef963a2e5751adeaa1bcdd141609a24727a
YoungHunCho <cyh905@nate.com>
2016-04-02 22:33:22 +0900

    friendly
    .

--- src/cppProject/Genie/src/Genie_Datamanagement.cpp
+++ src/cppProject/Genie/src/Genie_Datamanagement.cpp
@@ -14,3 +14,20 @@ using namespace std;
 	
 	}
 */
+
+//by cho 04.02
+int Genie_Datamanagement::getFriendly() {
+	return friendly;
+}
+
+void Genie_Datamanagement::setFriendly(const int num) {
+	friendly = num;
+}
+
+void Genie_Datamanagement::upFriendly(const int num = 1) {
+	firendly += num;
+}
+
+void Genie_Datamanagement::downFriendly(const int num = 1) {
+	friendly -= num;
+}
