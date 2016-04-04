4a05821a0c7a71654f40014686dcd4f105ed13be
YoungHunCho <cyh905@nate.com>
2016-04-05 05:02:45 +0900

    sreen capture
    .

--- src/cppProject/Genie/header/Genie_Capture.h
+++ src/cppProject/Genie/header/Genie_Capture.h
@@ -1,6 +1,10 @@
 #ifndef __GENIE_CAPTURE__
 #define __GENIE_CAPTURE__
 
+#define _CRT_SECURE_NO_WARNINGS
+#define _AFXDLL
+#include <afxwin.h>
+
 class Genie_Capture
 {
 	private:
@@ -11,6 +15,9 @@ class Genie_Capture
 	/*	...
 		if you want add on
 	*/	
+
+		//by cho 04.05
+		void screen_capture(std::string path);
 };
 
 #endif
