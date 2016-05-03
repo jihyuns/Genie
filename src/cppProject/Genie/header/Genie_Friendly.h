de81346b2b4d5f1e404b16fe1dd0868445de5cdd
DongDongJu <commisori28@gmail.com>
2016-05-03 18:16:38 +0900

    add Friendly

---
+++ src/cppProject/Genie/header/Genie_Friendly.h
@@ -0,0 +1,15 @@
+#ifndef __GENIE_FRIENDLY__
+#define __GENIE_FRIENDLY__
+#include <string>
+
+class Genie_Friendly
+{
+
+	private:
+
+	public:
+
+	string* KoreanStemmingFunc(string input);
+};
+
+#endif
---
+++ src/cppProject/Genie/src/Genie_Friendly.cpp
@@ -0,0 +1,7 @@
+#include "Genie_Friendly.h"
+#include <iostream>
+
+string* Genie_Friendly::KoreanStemmingFunc(string input)
+{
+	
+}
